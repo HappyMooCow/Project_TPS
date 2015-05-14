ff5cc94f7ee6ca5777793ab07a8f5c725c9cef10
Josh <De4thNite@gmail.com>
2015-05-14 20:19:46 +1000

    Initial Unity Project Commit
    Empty unity project with camera and crosshair scripts

---
+++ Project_TPS/Assets/Scripts/camera.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+	//Tween levels for zoom of camera
+	public int[] tween = new int[4];
+	public GameObject target;
+
+	//settings
+	public float hOffset; //horizontal offset += right -= left
+	public float vOffset; //vertical offset += down -= up
+	public float behind; //trailing distance of camera
+	public float speed; //movement speed
+	public float maxP; //max proportion of lerp used for slow stop
+	public float minD; //min distance before stopping camera
+
+	// Use this for initialization
+	void Start () {
+		transform.position = get_toLoc ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 toLoc = get_toLoc ();
+		float distance = Vector3.Distance (transform.position, toLoc);
+
+
+		if (speed / distance * Time.deltaTime > maxP) {
+			Debug.Log((Time.deltaTime * speed) / distance);
+			if (distance < minD) {
+				transform.position = get_toLoc ();
+			} else {
+				Vector3.Lerp (
+				transform.position,
+				toLoc, 
+				maxP * Time.deltaTime);
+			}
+		} else {
+			transform.position = Vector3.Lerp (
+			transform.position, 
+			toLoc,
+			speed / distance * Time.deltaTime);
+			
+		}
+	}
+
+	Vector3 get_toLoc() {
+		Vector3 output = new Vector3 (
+			target.transform.position.x + hOffset,
+			target.transform.position.y + vOffset,
+			target.transform.position.z - behind);
+
+		return output;
+	}
+
+}
---
+++ Project_TPS/Assets/Scripts/crosshair.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crosshair : MonoBehaviour {
+	public static float size = 0.1f;
+	public static float bloom = 0f;
+	public float sizeMin = 0.5f;
+	public float sizeMax = 1f;
+	public Texture2D crossTexture;
+	//public GameObject emitter = Camera.main;
+	public GameObject prefab;
+
+
+
+	// Use this for initialization
+	void OnGUI () {
+		Vector3 center = Camera.main.ViewportToScreenPoint (new Vector3 (0.5f, 0.5f, 0f));
+		float drawSize = sizeMax * size;
+
+		GUI.DrawTexture (new Rect (center.x - drawSize /2, center.y - drawSize /2, drawSize, drawSize), crossTexture);
+
+
+	}
+	
+
+}
